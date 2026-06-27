5b44e2646294aeb1d61c55a0b27119c11971b481
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-06-27 16:46:12 +0530

    variables

--- day1.py
+++ day1.py
@@ -2,4 +2,26 @@ print("Hello World!")
 
 #indentation
 if 5>2:
-    print(" five is greater the two")
+    print("five is greater the two")
+
+
+#print statements
+#multiple statements
+print("Hello My Name is Bhoomika")
+print("I am", 21, "years old!")
+
+#python variables
+x=5
+y="Bhoo"
+print(x)
+print(y)
+
+#type od datatype
+print(type(x))
+print(type(y))
+
+#case-sensitive
+A=34
+a=34
+print(A)
+print(a)

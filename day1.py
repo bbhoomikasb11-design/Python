8ea4845d6d157cd1add7fb0d027487c1dc72afc4
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-06-27 17:33:51 +0530

    variables-new

--- day1.py
+++ day1.py
@@ -25,3 +25,22 @@ A=34
 a=34
 print(A)
 print(a)
+
+#many values to multiple variables
+x1,y1,z1 = "orange", "apple","kiwi"
+print(x1)
+print(y1)
+print(z1)
+
+#one value to multiple variables
+x2=y2=z2 = "Orange"
+print(x2)
+print(y2)
+print(z2)
+
+#unpacking variables
+fruits = ["orange", "Kiwi", "Apple"]
+x3,y3,z3 = fruits
+print(x3)
+print(y3)
+print(z3) 

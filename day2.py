0d6b5b8558ec05cad221460a6db755af98e83538
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-06-28 19:47:09 +0530

    operators-2

--- day2.py
+++ day2.py
@@ -11,7 +11,7 @@ print(a%b)
 print(a//b)
 print(a**b)
 
-#assignment operators
+#comparision operators
 print(a==b)
 print(a!=b)
 print(a>b)
@@ -19,8 +19,31 @@ print(a<b)
 print(a<=b)
 print(a>=b)
 
+#ternary operators
+num = 6
+s= "WEEKDAY" if num<=5 else "WEEKEND"
+print(s)
 
+#logical operators
+print(a>5 and b<3)
+print(a>5 or b<3)
+print(not(a>4))
 
+#identity operators
+print(a is b)
+print(a is not b)
 
+#membership operators
+x = ["banana", "apple", "orange"]
+print("banana" in x)
+print("pineapple" not in x)
 
+#bitwise operator
+print(6&3)
+print(6|3)
+print(6^3)
+print(~6)
 
+#operator precedence
+print((100+1)-(100+1))
+print(5+4-6+4*6)

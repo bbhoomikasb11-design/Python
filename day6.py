5f363a5be8fa3ca73c3bc0ef64044e32652a1f01
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-07-03 19:52:32 +0530

    two

--- day6.py
+++ day6.py
@@ -4,10 +4,22 @@ def table():
     for i in range(1, 11):
         print(num, "x", i, "=", num * i)
 
-# 6. Factorial
 def factorial():
     n = 5
     fact = 1
     for i in range(1, n + 1):
         fact *= i
     print("Factorial =", fact)
+
+    
+def reverse_string():
+    text = "Python"
+    print("Reversed String:", text[::-1])
+
+def count_vowels():
+    text = "Hello World"
+    count = 0
+    for char in text.lower():
+        if char in "aeiou":
+            count += 1
+    print("Vowels:", count)

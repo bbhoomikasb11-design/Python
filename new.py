f7aa0d184a11128a74987a77f4c41d6cb9d8e9cb
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-07-30 10:32:55 +0530

    again

---
+++ new.py
@@ -0,0 +1,13 @@
+name = "Bhoomika"
+age = 21
+college = "Don Bosco Institute of Technology"
+branch = "AI&DS"
+cgpa = 8.95
+Dream_Company = "Microsoft & Google"
+
+print(name)
+print(age)
+print(college)
+print(branch)
+print(cgpa)
+print(Dream_Company)

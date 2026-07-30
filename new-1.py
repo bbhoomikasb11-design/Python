aceaf625be01abfe32043a03c95dff87f16d58df
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-07-30 10:50:14 +0530

    new

---
+++ new-1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print("\n----- Student Details -----")
+print(f"Name    : {name}")
+print(f"Age     : {age}")
+print(f"College : {college}")
+print(f"CGPA    : {cgpa}")
---
+++ new-2.py
@@ -0,0 +1,13 @@
+name = input("enter yout name")
+print(f"Hello {name}")
+
+a = 10
+b = 15
+print(a+b)
+
+year = 2026
+curr_year = int(input("enter your birth year: "))
+print(year - curr_year)
+
+r = int(input("enter the radius: "))
+print(3.14 * r *r)

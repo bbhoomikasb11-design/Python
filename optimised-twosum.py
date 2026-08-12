4ac4d8f6e9d59e70a9fba7ac450b01ccbc0eb0a1
bbhoomikasb11-design <bbhoomikasb11@gmail.com>
2026-08-12 11:59:33 +0530

    optimised-twosum

---
+++ optimised-twosum.py
@@ -0,0 +1,10 @@
+nums = [2, 7, 11, 15]
+target = 9
+def twosum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        needed = target - num
+        if needed in seen:
+            return [seen[needed], i]
+        seen[num]= i
+print(twosum(nums, target))
